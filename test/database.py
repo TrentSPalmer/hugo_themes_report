ec3f66eebf994236d41c53ddbc09336b2837bd43
Trent Palmer <trenttdually@gmail.com>
2021-08-23 20:13:14 -0700

    add get_themes_orderedby_cname() to database.py

--- test/database.py
+++ test/database.py
@@ -2,19 +2,18 @@ from rank_hugo_themes import (
     engine, Hugothemes,
     Hugothemes_from_gitlab, sessionmaker
 )
+from sqlalchemy import asc, func
 
 
 def get_theme_count():
     session = sessionmaker(bind=engine)()
     theme_count = session.query(Hugothemes).count()
-    print(theme_count)
     return theme_count
 
 
 def get_theme_count_from_gitlab():
     session = sessionmaker(bind=engine)()
     theme_count_from_gitlab = session.query(Hugothemes_from_gitlab).count()
-    print(theme_count_from_gitlab)
     return theme_count_from_gitlab
 
 
@@ -33,6 +32,12 @@ def get_newest_update_time_from_gitlab():
     ).order_by(
         Hugothemes_from_gitlab.commit_date_in_seconds.desc()).first()
     return newest_commit_time
+
+
+def get_themes_orderedby_cname():
+    session = sessionmaker(bind=engine)()
+    return session.query(
+        Hugothemes).order_by(asc(func.lower(Hugothemes.cname))).all()
 
 
 def get_themes():
